f43e9f354137be9fe21632312209ff0ffee47a64
Levi Goodwin <levixgoodwin13@gmail.com>
2018-01-24 08:04:27 -0700

    4w

--- Source.cpp
+++ Source.cpp
@@ -8,3 +8,30 @@
 //
 //advanced: make an account at HackerRank.com
 //do this problem: https://www.hackerrank.com/challenges/c-tutorial-struct/problem
+namespace robloxplayer239
+{
+	int rand() {
+		return 5;
+	}
+}
+
+#include <iostream>
+#include<time.h>
+
+int main() {
+	srand(time(NULL));
+	int randNum;
+	int fakeRandNum;
+
+	randNum = rand();
+	fakeRandNum = robloxplayer239::rand();
+
+	std::cout << randNum << std::endl;
+	std::cout << fakeRandNum << std::endl;
+	system("pause");
+
+
+
+
+
+}
